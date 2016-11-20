f7bf493edb538ca09b39d03e3b147c11bae2843c
Daan Kloetstra <daankloetstra@hotmail.com>
2016-11-20 12:09:18 +0100

    Working menu with touch

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,24 +5,90 @@
 #include <MI0283QT9.h>
 #include <digitalWriteFast.h>
 #include <stdbool.h>
+#include <Arduino.h>
+#include <EEPROM.h>
 
 MI0283QT9 lcd;
+
+String gameName = "Bomberman";
 bool inGame = false;
 
+// void writeCalData(void)
+// {
+//   uint16_t i, addr=0;
+//   uint8_t *ptr;
+//
+//   EEPROM.write(addr++, 0xAA);
+//
+//   ptr = (uint8_t*)&lcd.tp_matrix;
+//   for(i=0; i < sizeof(CAL_MATRIX); i++)
+//   {
+//     EEPROM.write(addr++, *ptr++);
+//   }
+//
+//   return;
+// }
+//
+//
+uint8_t readCalData(void)
+{
+  uint16_t i, addr=0;
+  uint8_t *ptr;
+  uint8_t c;
+
+  c = EEPROM.read(addr++);
+  if(c == 0xAA)
+  {
+    ptr = (uint8_t*)&lcd.tp_matrix;
+    for(i=0; i < sizeof(CAL_MATRIX); i++)
+    {
+      *ptr++ = EEPROM.read(addr++);
+    }
+    return 0;
+  }
+
+  return 1;
+}
+
 void initMenu() {
+  lcd.fillScreen(RGB(0,0,0));
+  lcd.drawText(20, 20, gameName, RGB(255,255,255), RGB(0,0,0), 4);
+
+  String startText = "Start game";
+  lcd.drawText(80, 100, startText, RGB(255,255,255), RGB(30,130,76), 2);
 
 }
 
 void initGame() {
-
+  lcd.fillScreen(RGB(255,255,255));
 }
 
 
 int main(void) {
+  init();
   lcd.begin(4);
-  lcd.fillScreen(RGB(255,255,255));
+
+  // calibration for touchpanel
+  readCalData();
+  // //set touchpanel calibration data
+  // lcd.touchRead();
+  // if(lcd.touchZ() || readCalData()) //calibration data in EEPROM?
+  // {
+  //   lcd.touchStartCal(); //calibrate touchpanel
+  //   writeCalData(); //write data to EEPROM
+  // }
+
+  initMenu();
   for (;;) {
-    /* code */
+    if (!inGame && lcd.touchRead() == 1) {
+      int_least16_t touchX = lcd.touchX();
+      int_least16_t touchY = lcd.touchY();
+      if (touchX > 108 && touchX < 234 && touchY > 98 && touchY < 117) {
+        initGame();
+        inGame = true;
+      }
+    }
+
   }
   return 0;
 }
