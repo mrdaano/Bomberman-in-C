4b0cce53d557a8556f746433755daa4b7c01c0c2
Daan Kloetstra <daankloetstra@hotmail.com>
2016-11-21 22:34:58 +0100

    Added comments

--- src/Game.cpp
+++ src/Game.cpp
@@ -39,7 +39,7 @@ bool inGame = false;
 //   return;
 // }
 //
-//
+// Read the calibration data from EEPROM for the touch input
 uint8_t readCalData(void)
 {
   uint16_t i, addr=0;
@@ -47,10 +47,10 @@ uint8_t readCalData(void)
   uint8_t c;
 
   c = EEPROM.read(addr++);
-  if(c == 0xAA)
+  if(c == 0xAA) // Start byte for calibration data
   {
     ptr = (uint8_t*)&lcd.tp_matrix;
-    for(i=0; i < sizeof(CAL_MATRIX); i++)
+    for(i=0; i < sizeof(CAL_MATRIX); i++) // Loop trough the calibration data in EEPROM and store it in the CAL_MATRIX of the GraphicsLib
     {
       *ptr++ = EEPROM.read(addr++);
     }
@@ -61,6 +61,7 @@ uint8_t readCalData(void)
 }
 
 void placePlayers() {
+  // Init player 1
   player1.x = 10;
   player1.y = 10;
   player1.color = RGB(30,130,76);
@@ -69,6 +70,7 @@ void placePlayers() {
 }
 
 void initMenu() {
+  // Remove every content on screen
   lcd.fillScreen(RGB(0,0,0));
   lcd.drawText(20, 20, gameName, RGB(255,255,255), RGB(0,0,0), 4);
 
@@ -78,7 +80,9 @@ void initMenu() {
 }
 
 void initGame() {
+  // Remove every content on screen
   lcd.fillScreen(RGB(255,255,255));
+  // Create all the walls
   for (int w = 0; w < maxBlocksInWidth; w++) {
     for (int i = 0; i < maxBlocksInLength; i++) {
       lcd.fillRect(w * 57 + 30, i * 57 + 20, 30, 30, RGB(0,0,0));
@@ -105,10 +109,12 @@ int main(void) {
 
   initMenu();
   for (;;) {
+    // If player is not in game and user touched the screen
     if (!inGame && lcd.touchRead() == 1) {
       int_least16_t touchX = lcd.touchX();
       int_least16_t touchY = lcd.touchY();
       if (touchX > 108 && touchX < 234 && touchY > 98 && touchY < 117) {
+        // If button is pressed go to game init
         initGame();
         inGame = true;
       }
